a4b1b5ae561257b7aae45b0ec84537dd234bb01f
Tim Caswell <tim@creationix.com>
2015-02-20 12:47:03 -0600

    Update deps to fix networking bugs

--- package.lua
+++ package.lua
@@ -4,8 +4,9 @@ return {
   dependencies = {
     "creationix/git@0.1.0",
     "creationix/require@1.0.2",
-    "creationix/coro-tcp@1.0.1",
+    "creationix/coro-tcp@1.0.3",
     "creationix/coro-fs@1.2.3",
+    "creationix/coro-wrapper@0.1.0",
     "creationix/pretty-print@0.1.0",
     "luvit/http-codec@0.1.0",
   },
