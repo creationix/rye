b5b521f483f5aa15b9365022455b85696fbcc03a
Tim Caswell <tim@creationix.com>
2015-02-24 21:37:25 -0600

    Update deps

--- package.lua
+++ package.lua
@@ -2,12 +2,12 @@ return {
   name = "creationix/rye",
   version = "0.0.1",
   dependencies = {
+    "luvit/require@0.2.1",
+    "luvit/http-codec@0.1.3",
+    "luvit/pretty-print@0.1.0",
     "creationix/git@0.1.0",
-    "luvit/require@0.2.0",
     "creationix/coro-tcp@1.0.3",
     "creationix/coro-fs@1.2.3",
     "creationix/coro-wrapper@0.1.0",
-    "creationix/pretty-print@0.1.0",
-    "luvit/http-codec@0.1.0",
   },
 }
